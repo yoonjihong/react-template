bcd920bff868fe30f8741511ef346e98ca5d5713
윤지홍 <47561303+yoonjihong@users.noreply.github.com>
2022-08-28 00:07:27 +0900

    초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import Counter from './Components/Counter';
-
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
